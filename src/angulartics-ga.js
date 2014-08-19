1219f3c6d154932f3d5f16da8d977aa25d710c3e
Jeroen Willemsen <jwillemsen@xebia.com>
2014-08-19 22:41:10 +0200

    first prototype of eventTrackAndWait for ga

--- src/angulartics-ga.js
+++ src/angulartics-ga.js
@@ -4,58 +4,110 @@
  * Universal Analytics update contributed by http://github.com/willmcclellan
  * License: MIT
  */
-(function(angular) {
-'use strict';
-
-/**
- * @ngdoc overview
- * @name angulartics.google.analytics
- * Enables analytics support for Google Analytics (http://google.com/analytics)
- */
-angular.module('angulartics.google.analytics', ['angulartics'])
-.config(['$analyticsProvider', function ($analyticsProvider) {
-
-  // GA already supports buffered invocations so we don't need
-  // to wrap these inside angulartics.waitForVendorApi
-
-  $analyticsProvider.settings.trackRelativePath = true;
-
-  $analyticsProvider.registerPageTrack(function (path) {
-    if (window._gaq) _gaq.push(['_trackPageview', path]);
-    if (window.ga) ga('send', 'pageview', path);
-  });
+(function (angular){
+  'use strict';
 
   /**
-   * Track Event in GA
-   * @name eventTrack
-   *
-   * @param {string} action Required 'action' (string) associated with the event
-   * @param {object} properties Comprised of the mandatory field 'category' (string) and optional  fields 'label' (string), 'value' (integer) and 'noninteraction' (boolean)
-   *
-   * @link https://developers.google.com/analytics/devguides/collection/gajs/eventTrackerGuide#SettingUpEventTracking
-   *
-   * @link https://developers.google.com/analytics/devguides/collection/analyticsjs/events
+   * @ngdoc overview
+   * @name angulartics.google.analytics
+   * Enables analytics support for Google Analytics (http://google.com/analytics)
    */
-  $analyticsProvider.registerEventTrack(function (action, properties) {
-    // GA requires that eventValue be an integer, see:
-    // https://developers.google.com/analytics/devguides/collection/analyticsjs/field-reference#eventValue
-    // https://github.com/luisfarzati/angulartics/issues/81
-    if(properties.value) {
-      var parsed = parseInt(properties.value, 10);
-      properties.value = isNaN(parsed) ? 0 : parsed;
-    }
+  angular.module('angulartics.google.analytics', ['angulartics'])
+    .config(['$analyticsProvider', function ($analyticsProvider, $q){
 
-    if (window._gaq) {
-      _gaq.push(['_trackEvent', properties.category, action, properties.label, properties.value, properties.noninteraction]);
-    }
-    else if (window.ga) {
-      if (properties.noninteraction) {
-        ga('send', 'event', properties.category, action, properties.label, properties.value, {nonInteraction: 1});
-      } else {
-        ga('send', 'event', properties.category, action, properties.label, properties.value);
-      }
-    }
-  });
+      // GA already supports buffered invocations so we don't need
+      // to wrap these inside angulartics.waitForVendorApi
 
-}]);
+      $analyticsProvider.settings.trackRelativePath = true;
+
+      $analyticsProvider.registerPageTrack(function (path){
+        if (window._gaq) _gaq.push(['_trackPageview', path]);
+        if (window.ga) ga('send', 'pageview', path);
+      });
+
+      /**
+       * Track Event in GA
+       * @name eventTrack
+       *
+       * @param {string} action Required 'action' (string) associated with the event
+       * @param {object} properties Comprised of the mandatory field 'category' (string) and optional  fields 'label' (string), 'value' (integer) and 'noninteraction' (boolean)
+       *
+       * @link https://developers.google.com/analytics/devguides/collection/gajs/eventTrackerGuide#SettingUpEventTracking
+       *
+       * @link https://developers.google.com/analytics/devguides/collection/analyticsjs/events
+       */
+      $analyticsProvider.registerEventTrack(function (action, properties){
+        // GA requires that eventValue be an integer, see:
+        // https://developers.google.com/analytics/devguides/collection/analyticsjs/field-reference#eventValue
+        // https://github.com/luisfarzati/angulartics/issues/81
+        if (properties.value) {
+          var parsed = parseInt(properties.value, 10);
+          properties.value = isNaN(parsed) ? 0 : parsed;
+        }
+
+        if (window._gaq) {
+          _gaq.push(['_trackEvent', properties.category, action, properties.label, properties.value, properties.noninteraction]);
+        }
+        else if (window.ga) {
+          if (properties.noninteraction) {
+            ga('send', 'event', properties.category, action, properties.label, properties.value, {nonInteraction: 1});
+          } else {
+            ga('send', 'event', properties.category, action, properties.label, properties.value);
+          }
+        }
+      });
+
+      /**
+       * Track Event in GA (in prototype stage!)
+       * @name eventTrackAndWait
+       *
+       * @param {string} action Required 'action' (string) associated with the event
+       * @param {object} properties Comprised of the mandatory field 'category' (string) and optional  fields 'label' (string), 'value' (integer) and 'noninteraction' (boolean)
+       *
+       * currently only supports ga!!!
+       */
+      $analyticsProvider.registerEventTrackAndWait(function (action, properties){
+        // GA requires that eventValue be an integer, see:
+        // https://developers.google.com/analytics/devguides/collection/analyticsjs/field-reference#eventValue
+        // https://github.com/luisfarzati/angulartics/issues/81
+        if (properties.value) {
+          var parsed = parseInt(properties.value, 10);
+          properties.value = isNaN(parsed) ? 0 : parsed;
+        }
+
+        if (window.ga) {
+          var deferred = $q.defer();
+          if (properties.noninteraction) {
+            ga('send', 'event', {
+              'eventCategory': properties.category,
+              'eventAction': action,
+              'eventLabel': properties.label,
+              'eventValue': properties.value,
+              'nonInteraction': 1,
+              'hitCallback': function (){
+                deferred.resolve();
+              },
+              'hitCallbackFail': function (){ //not sure about this one yet...
+                deferred.reject();
+              }
+            });
+          } else {
+            ga('send', 'event', {
+              'eventCategory': properties.category,
+              'eventAction': action,
+              'eventLabel': properties.label,
+              'eventValue': properties.value,
+              'hitCallback': function (){
+                deferred.resolve();
+              },
+              'hitCallbackFail': function (){ //not sure about this one yet...
+                deferred.reject();
+              }
+            });
+
+          }
+          return deferred.promise;
+        }
+      });
+    }]);
 })(angular);
